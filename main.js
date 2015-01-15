f856e67dedf6ee20b7f7b43f7cff4f3cf38229af
Ed Cottrell <emc@edcottrell.com>
2015-01-15 12:39:26 -0600

    Update main.js
    Fix CCA links
    Fix links for cases with multiple docket numbers
    General code cleanup (refactoring into more functions, better variable names)
    Add "go to star page" when user presses *
    Focus on search box when user presses /

--- main.js
+++ main.js
@@ -22,16 +22,70 @@ var dissentBorder = '4px solid #ff3333',
     footnoteReferenceBackgroundColor = '#ffdddd',
     footnoteReferenceBorderThickness = '1px',
     footnoteReferenceBorderType = 'solid',
-    footnoteReferenceBorderColor = '#ffcccc';
+    footnoteReferenceBorderColor = '#ffcccc',
+    $courtBlock = $('.co_courtBlock'),
+    $docketBlock = $('.co_docketBlock'),
+    docketBlockHTML = $docketBlock.length? $docketBlock.html() : null;
+
+/**
+ * Create a form for handling Texas CCA "links"
+ */
+function initTexasCCAForm() {
+    var txCCAForm,
+        CaseNumberNo;
+    if(!document.getElementsByName('txCCAForm').length) {
+        txCCAForm = document.createElement('form');
+        txCCAForm.name = 'txCCAForm';
+        txCCAForm.action = 'http://www.cca.courts.state.tx.us/opinions/casesearch.asp';
+        txCCAForm.target = '_blank';
+        document.getElementsByTagName("body")[0].appendChild(txCCAForm);
+        if(!document.getElementsByName('CaseNumberNo').length) {
+            CaseNumberNo = document.createElement('input');
+            CaseNumberNo.name = 'CaseNumberNo';
+            document.getElementsByName("txCCAForm")[0].appendChild(CaseNumberNo);
+        }
+    }
+}
+
+/**
+ * Handle Texas TAMES links
+ */
+function goToTexasTAMESLink(caseNo, isCCA) {
+    var domain = "http://www.search.txcourts.gov/", /* Domain for the searches */
+	    path = "CaseSearch.aspx?coa=cossup"; /* Initialize the URL to use if we cannot do a search */
+    
+    caseNo = realTexasCaseNumberFromString(caseNo);
+    
+    if(caseNo != "") {
+        path = "Case.aspx?cn="+caseNo;
+    }
+    window.open(domain + path);
+}
+
+/**
+ * Is it a Texas Court of Criminal Appeals case?
+ */
+function isTexasCCACase() {
+    return $courtBlock && $courtBlock.text().match(/Court of Criminal Appeals of Texas/);
+}
+
+/**
+ * Is it a Texas case in the Supreme Court of Texas or Courts of Appeals?
+ */
+function isTexasNonCCACase() {
+    return $courtBlock && $courtBlock.text().match(/(?:Supreme Court|Court of (?:Civil )?Appeals) of Texas/);
+}
 
 /* Get the real Texas case number (no leading "No.", for example) from a string */
-function RealCaseNumberFromString(caseNo) {
+function realTexasCaseNumberFromString(caseNo) {
+    var caseNoParts;
+    
     /* Replace en-dashes with hyphens and strip any leading or trailing characters */
-	caseNo = caseNo.replace(/\u2013/g, "-").replace(/^[^\d]+|[^CRV\d]+$/g, "");
+	caseNo = caseNo.replace(/\u2013/g, "-");
 
     /* If the case is a -CR or -CV case, make sure the numeric parts are appropriately padded with leading zeroes */
-	if(caseNo.match(/C[RV]$/)) {
-		var caseNoParts = caseNo.split("-");
+	if (caseNo.match(/C[RV]$/)) {
+		caseNoParts = caseNo.split("-");
 		caseNoParts[0] = ("00"+caseNoParts[0]).slice(-2);
 		caseNoParts[1] = ("00"+caseNoParts[1]).slice(-2);
 		caseNoParts[2] = ("00000"+caseNoParts[2]).slice(-5);
@@ -43,52 +97,16 @@ function RealCaseNumberFromString(caseNo) {
 
 $(function() { /* Don't run until jQuery is loaded */
     /* See if it's a Texas case */
-    var cblock = $('.co_courtBlock');
-    if( cblock && cblock.text().match(/(?:Supreme Court|Court of (?:Civil )?Appeals) of Texas/) ) {
-        /* It is (either COA or SCTEX), so make the case number clickable */
-        var domain = "http://www.search.txcourts.gov/", /* Domain for the searches */
-	    path = "CaseSearch.aspx?coa=cossup"; /* Initialize the URL to use if we cannot do a search */
-            caseNoBlock = $('.co_docketBlock');
-        if(caseNoBlock) {
-            var caseNo = RealCaseNumberFromString(caseNoBlock.text());
-            /* If the case number is non-empty, get the URL for the search page. */
-            if(caseNo != "") {
-                path = "Case.aspx?cn="+caseNo;
-            }
-            caseNoBlock
-                .css({color:'blue', 'text-decoration' : 'underline', 'cursor': 'pointer'})
-                .on('click', function() {
-                    window.open(domain+path);
-                });
+    if (docketBlockHTML && (isTexasCCACase() || isTexasNonCCACase())) {
+        $docketBlock.html(docketBlockHTML.replace(/((?:<[^>]+>|Nos?\.\s*|,\s*)+)([^,.]+)/g, '$1<span class="researchToolsTexasTAMESLink">$2</span>'));
+        $('.researchToolsTexasTAMESLink')
+            .css({color:'blue', 'text-decoration' : 'underline', 'cursor': 'pointer'})
+            .on('click', function() {
+                goToTexasTAMESLink($(this).text(), isTexasCCACase());
+            });
+        if (isTexasCCACase()) {
+            initTexasCCAForm();
         }
-    }
-    else if( cblock && cblock.text().match(/Court of Criminal Appeals of Texas/) ) {
-	/* It's a Texas CCA case; set up a form to submit the search */
-	var caseNoBlock = $('.co_docketBlock');
-	if(caseNoBlock) {
-		if(!document.getElementsByName('txCCAForm').length) {
-			var txCCAForm = document.createElement('form');
-			txCCAForm.name = 'txCCAForm';
-			txCCAForm.action = 'http://www.cca.courts.state.tx.us/opinions/casesearch.asp';
-			txCCAForm.target = '_blank';
-			document.getElementsByTagName("body")[0].appendChild(txCCAForm);
-			if(!document.getElementsByName('CaseNumberNo').length) {
-				var CaseNumberNo = document.createElement('input');
-				CaseNumberNo.name = 'CaseNumberNo';
-				document.getElementsByName("txCCAForm")[0].appendChild(CaseNumberNo);
-			}
-		}
-		var caseNo = RealCaseNumberFromString(caseNoBlock.text());
-		/* If the case number is non-empty, get the URL for the search page. */
-		if(caseNo != "") {
-			caseNoBlock
-				.css({color:'blue', 'text-decoration' : 'underline', 'cursor': 'pointer'})
-				.on('click', function() {
-					document.getElementsByName("CaseNumberNo")[0].value = caseNo;
-					document.getElementsByName("txCCAForm")[0].submit();
-				});
-		}
-	}
     }
     
     /* Highlight concurrences and dissents */
@@ -126,6 +144,11 @@ $(function() { /* Don't run until jQuery is loaded */
          '        background-color: ' + footnoteReferenceBackgroundColor + ';',
          '        border: ' + footnoteReferenceBorderThickness + ' ' + footnoteReferenceBorderType + ' ' + footnoteReferenceBorderColor + ';',
          '    }',
+         '    .co_docketBlock span.researchToolsTexasTAMESLink {',
+         '        padding-right: 0px;',
+         '        margin-right: 0px;',
+         '        border-right: none;',
+         '    }',
          '</style>'].join("\n")
     );
     
@@ -154,4 +177,32 @@ $(function() { /* Don't run until jQuery is loaded */
                 }
             }
         );
+    
+    
+    /**
+     * Jump to a star page if the user presses / or * anywhere but a search box
+     */
+    $('body').on('keypress', function(e) {
+        var tag = e.target.tagName.toLowerCase(),
+            page,
+            $page,
+            $doc = $('#co_document_0'),
+            docTop = $doc.length ? $doc.offset().top : 0;
+            
+        if (tag != 'textarea' && tag != 'input') {
+            if (e.keyCode === 42 && $doc.length) { // *
+                e.preventDefault();
+                e.stopPropagation();
+                page = parseInt(prompt('Enter the page to go to:'));
+                $page = $('.co_starPage:contains("' + page + '")');
+                if ($page.length) {
+                    $('body').animate({ scrollTop: $page.offset().top - docTop  }, 500);
+                }
+            } else if (e.keyCode === 47) { // /
+                e.preventDefault();
+                e.stopPropagation();
+                $('#searchInputId').focus();
+            }
+        }
+	});
 });
